36d654802f8a79e4838e841de16e8650e558319b
sylvain-mihut <sylvain.mihut@laplateforme.io>
2022-11-14 15:57:59 +0100

    commit job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+// créations de variables
+
+// variable 001 : str
+$str = "LaPlateforme";
+echo "$str";
+
+// variable 002 : str2
+$str2 = "Vive";
+echo "$str2";
+
+// variable 003 : str3
+$str3 = "!";
+echo "$str3";
+
+// création de retour a la ligne
+echo "<br>";
+
+// affichage de phrase avec espaces 
+echo "$str2", "\n", "$str", "\n", "$str3";
+
+// retour a la ligne
+echo "<br>";
+
+// nouvelle variable
+$val = "6";
+
+// faire une addition avec ma valeur "val"
+echo "$val" + "4";
+
+// retour a la ligne
+echo "<br>";
+
+// nouvelle variable
+$myBool = true;
+
+// echo variable "$myBool"
+echo "La valeur quand égal true $myBool";
+
+echo "<br>";
+
+// modification de "$myBool"
+$myBool = false;
+
+echo "La valeur quand égal false $myBool";
+
+?>
