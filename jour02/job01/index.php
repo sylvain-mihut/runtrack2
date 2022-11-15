45e47cfd046d3643ba88b057959c6fce5e3216fe
sylvain-mihut <sylvain.mihut@laplateforme.io>
2022-11-15 11:37:39 +0100

    commit for days2 job 01

---
+++ jour02/job01/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+// création de boucle
+
+for ($i = 0 ; $i <= 1337; $i++){
+    // echo "nombre : $i <br>";
+    if( $i == 42 ){
+        echo "<b><u> je suis gras et souligné : $i </b></u>";
+        echo "<br>";
+    }
+    else{
+        echo "$i";
+        echo "<br>";
+    }
+}
+
+    
