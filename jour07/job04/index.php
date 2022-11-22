85824828d7d872d7a6dfede73778e59a4028ca5e
sylvain-mihut <sylvain.mihut@laplateforme.io>
2022-11-22 11:39:05 +0100

    day07-job04

---
+++ jour07/job04/index.php
@@ -0,0 +1,31 @@
+<!-- Créez une fonction nommée “calcule()” qui prend 3 paramètres :
+● le premier, “$a”, est un nombre,
+● le deuxième, "$operation", est un caractère (string) contenant le type d’opération
+(+, -, *, /, %),
+● le troisième, “$b”, est un nombre.
+La fonction doit retourner le résultat de l’opération. -->
+
+<?php
+
+function calcule(int $a, string $operation, int $b){
+    if($operation === '+') {
+        $result= $a+$b;
+    }
+    elseif ($operation === '-') {
+        $result= $a-$b;
+    }
+    elseif($operation === '*') {
+        $result= $a*$b;
+    }
+    elseif($operation === '/') {
+        $result= $a/$b;
+    }
+    else{
+        $result="input incorect";
+    }
+    return $result;
+}
+
+echo calcule(2,'+',6);
+
+?>
